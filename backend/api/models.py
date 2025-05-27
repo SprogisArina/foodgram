728d84d91391aa2c567763e4b93208d2d12498db
Arina Sprogis <sprogis_a@outlook.com>
2025-05-27 23:41:18 +0500

    Add models

---
+++ backend/api/models.py
@@ -0,0 +1,127 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from .constants import MAX_LENGTH, MAX_STR_LENGTH
+
+
+User = get_user_model()
+
+
+class ProjectUser(AbstractUser):
+    pass
+
+
+class BaseModel(models.Model):
+    name = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Название'
+    )
+
+    def __str__(self):
+        return self.name[:MAX_STR_LENGTH]
+
+
+class Ingredient(BaseModel):
+    measurement_unit = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Единица измерения'
+    )
+
+    class Meta:
+        verbose_name = 'ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=MAX_LENGTH,
+        unique=True,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Уникальный идентификатор'
+    )
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name[:MAX_STR_LENGTH]
+
+
+class Recipe(BaseModel):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор рецепта'
+    )
+    image = models.ImageField(
+        upload_to='recipes/images/'
+    )
+    text = models.TextField(verbose_name='Описание')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientRecipe',
+        verbose_name='Ингредиенты'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        through='TagRecipe',
+        verbose_name='Тэги'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления'
+    )
+    pub_date = models.DateTimeField(verbose_name='Дата публикации')
+
+    class Meta:
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ('-created_at')
+        default_related_name = 'recipes'
+
+
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(
+        verbose_name='Количество ингредиента'
+    )
+
+    def __str__(self):
+        return f'{self.ingredient} {self.recipe}'
+
+
+class TagRecipe(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.tag} {self.recipe}'
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} {self.following}'
+
+
+class SelectedRecipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} {self.recipe}'
+
+
+class Favorite(SelectedRecipe):
+    pass
+
+
+class Cart(SelectedRecipe):
+    pass
