e2c6ca59bd92de901b6307a91934c2c22ccdb0e5
Arina Sprogis <sprogis_a@outlook.com>
2025-06-09 13:43:52 +0500

    Add RecipeFilter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,4 +1,36 @@
 from django_filters import rest_framework as filters
 
+from .models import Recipe, Tag
+
+
 class RecipeFilter(filters.FilterSet):
-    
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+        conjoined=True
+    )
+    is_favorited = filters.NumberFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.NumberFilter(
+        method='filter_is_in_shopping_cart'
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags')
+
+    def filter_base(self, queryset, name, value, related_name):
+        user = self.request.user
+
+        if value == 1 and user.is_authenticated:
+            return queryset.filter(**{f'{related_name}__user': user})
+
+        return queryset
+
+    def filter_is_favorited(self, queryset, name, value):
+        return self.filter_base(
+            queryset, name, value, related_name='favorites'
+        )
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        return self.filter_base(queryset, name, value, related_name='cart')
