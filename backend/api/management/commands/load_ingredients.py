54fe0260c47aaaabb789c44673ac9b95ddde086f
Arina Sprogis <sprogis_a@outlook.com>
2025-06-19 21:42:48 +0500

    Add load_ingredients

---
+++ backend/api/management/commands/load_ingredients.py
@@ -0,0 +1,19 @@
+import json
+from django.core.management.base import BaseCommand
+from api.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Load ingredients data'
+
+    def handle(self, *args, **options):
+        with open('data/ingredients.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            infredients = [
+                Ingredient(
+                    name=item['name'],
+                    measurement_unit=item['measurement_unit']
+                )for item in data
+            ]
+            Ingredient.objects.bulk_create(infredients)
+        self.stdout.write('Данные успешно загружены!')
